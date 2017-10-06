b4ef72aee893009a6325aede1f1de0ac43f27402
Clederson Dotti <cledersonsj@gmail.com>
2017-10-06 03:28:10 -0300

    layout new - version to gringo see

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,17 +13,9 @@
         <!-- Styles -->
         <link href="{{ asset('css/app.css') }}" rel="stylesheet">
         <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
-        
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #ffffff;
-                font-family: 'Raleway', sans-serif;
-                font-weight: 100;
-                height: 100vh;
-                margin: 0;
-            }
+        <link href="{{ asset('css/outdefault.css') }}" rel="stylesheet">
 
+        <style type="text/css">
             body {
               background: url({{ asset('img/bg.png')}} ) no-repeat center center fixed;
               -webkit-background-size: cover;
@@ -31,109 +23,6 @@
               background-size: cover;
               -o-background-size: cover;
             }
-
-            img {
-                max-width: 100%;
-                max-height: 100%;
-            }
-
-            button {
-                border-radius: 10px;
-                border-style: solid;
-                background: white;
-                color: blue;
-                text-decoration: bold;
-            }
-
-            input {
-                background: transparent;
-                border-radius: 20px;
-                border-color: white;
-                border-style: solid;
-                width:100% ;
-                text-align: right;
-            }
-
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 12px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-
-            .leftcol {
-                text-align:right;
-            }
-
-            .rightcol {
-                text-align:left;
-            }
-
-            .slothin {
-                font-size: 38px;
-            }
-            
-            .slothick {
-                font-size: 38px;
-                font-weight: bold;
-            }
-
-            .nopadright {
-                padding-right:0px;
-            }
-
-            .tip {
-                font-size: 10px;
-                color: white;
-            }
-
-            .thick {
-                font-weight: bold;
-            }
-            a.button {
-                -webkit-appearance: button;
-                -moz-appearance: button;
-                appearance: button;
-
-                text-decoration: none;
-                color: initial;
-            }
-
         </style>
     </head>
     <body>
@@ -159,7 +48,7 @@
                         <div class="form-group col-md-6 col-md-offset-6 nopadright">
                             <label for="email">Email</label>
                             <div>
-                                <input id="email" type="email" class="myinput" name="email" value="{{ old('email') }}" required autofocus>
+                                <input id="email" type="email" class="myinput" name="email" value="{{ old('email') }}" required autofocus disabled>
                                 @if ($errors->has('email'))
                                     <span class="help-block">
                                         <strong>{{ $errors->first('email') }}</strong>
@@ -170,7 +59,7 @@
                         <div class="form-group col-md-6 col-md-offset-6 nopadright {{ $errors->has('password') ? ' has-error' : '' }}">
                             <label for="password">Senha</label>
                             <div >
-                                <input id="password" type="password" class="myinput" name="password" required>
+                                <input id="password" type="password" class="myinput" name="password" required disabled>
                             </div>
                             
                         </div>
@@ -181,17 +70,19 @@
                         </div>
                         <div class="row">
                             <div class="col-md-6 col-md-offset-6">
-                                <button type="submit" class="btn btn-primary">LOGIN</button>
+                                <!-- <button type="submit" class="btn btn-primary">LOGIN</button> -->
                                 <a href="{{ route('register') }}" class="btn btn-primary">
                                     <span>SEJA CLUBENS</span>
                                 </a>
                             </div>
                         </div>
+                        <!--
                         <div class="row">
                             <div class="col-md-6 col-md-offset-3">
                                 <a href="{{ route('password.request') }}" class="thick tip">ESQUECEU SUA SENHA?</a>
                             </div>
                         </div>
+                        -->
                     </form>
                 </div>
                 <div class="col-md-6 rightcol">
