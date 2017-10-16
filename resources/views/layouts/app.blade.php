ce9aa175c5cab50e5324cf1e0a23d6e24e29441f
Vitor Lobao <vitor.lobs@gmail.com>
2017-10-15 22:23:21 -0200

    adicionando favicon e css nas pastas, css não incorporado

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,6 +5,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
+    <link rel="icon" type="image/png" href="/favicon.png" />
+
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
@@ -13,10 +15,13 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/bootstrap.min.css') }}" rel="stylesheet">
+{{--    <link href="{{ asset('css/form.css') }}" rel="stylesheet">--}}
+{{--    <link href="{{ asset('css/style.css') }}" rel="stylesheet">--}}
     <style>
         .alert-danger {
             background-color: rgba(255, 0, 16, 0.51);
         }
+
     </style>
 </head>
 <body>
